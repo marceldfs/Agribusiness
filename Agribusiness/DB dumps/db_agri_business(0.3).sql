ec3fdd59f10f79a8224987fe685c43afd70cb544
ayrtonjb <ayrton--jorge@hotmail.com>
2015-06-09 02:20:41 +0200

    Criar segunda versao da BD #17

---
+++ Agribusiness/DB dumps/db_agri_business(0.3).sql
@@ -0,0 +1,380 @@
+-- MySQL dump 10.13  Distrib 5.6.23, for Win32 (x86)
+--
+-- Host: localhost    Database: db_agribusiness
+-- ------------------------------------------------------
+-- Server version	5.5.0-m2-community
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
+  `Id` int(11) NOT NULL,
+  `Nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `distrito`
+--
+
+DROP TABLE IF EXISTS `distrito`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `distrito` (
+  `Id` int(11) NOT NULL,
+  `Id_provincia` int(11) DEFAULT NULL,
+  `Nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_id_provincia_idx` (`Id_provincia`),
+  CONSTRAINT `fk_id_provincia` FOREIGN KEY (`Id_provincia`) REFERENCES `provincia` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `distrito`
+--
+
+LOCK TABLES `distrito` WRITE;
+/*!40000 ALTER TABLE `distrito` DISABLE KEYS */;
+/*!40000 ALTER TABLE `distrito` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entidade`
+--
+
+DROP TABLE IF EXISTS `entidade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `entidade` (
+  `Id` int(11) NOT NULL,
+  `Nome` varchar(100) DEFAULT NULL,
+  `Identificador` varchar(45) DEFAULT NULL,
+  `Senha` varchar(45) DEFAULT NULL,
+  `DataAutenticacao` date DEFAULT NULL,
+  `Localizacao` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entidade`
+--
+
+LOCK TABLES `entidade` WRITE;
+/*!40000 ALTER TABLE `entidade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entidade` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entidade_servico`
+--
+
+DROP TABLE IF EXISTS `entidade_servico`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `entidade_servico` (
+  `id_entidade` int(11) NOT NULL,
+  `id_servico` int(11) NOT NULL,
+  PRIMARY KEY (`id_entidade`,`id_servico`),
+  KEY `fk_id_servico_idx` (`id_servico`),
+  CONSTRAINT `fk_id_entidadeES` FOREIGN KEY (`id_entidade`) REFERENCES `entidade` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_servicoES` FOREIGN KEY (`id_servico`) REFERENCES `servico` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entidade_servico`
+--
+
+LOCK TABLES `entidade_servico` WRITE;
+/*!40000 ALTER TABLE `entidade_servico` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entidade_servico` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grupo_utilizador`
+--
+
+DROP TABLE IF EXISTS `grupo_utilizador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grupo_utilizador` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grupo_utilizador`
+--
+
+LOCK TABLES `grupo_utilizador` WRITE;
+/*!40000 ALTER TABLE `grupo_utilizador` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grupo_utilizador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grupo_utilizador_permissao`
+--
+
+DROP TABLE IF EXISTS `grupo_utilizador_permissao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grupo_utilizador_permissao` (
+  `id` int(11) NOT NULL,
+  `id_grupoutilizador` int(11) NOT NULL,
+  `id_permissao` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_id_grupoutilizador_idx` (`id_grupoutilizador`),
+  KEY `fk_id_permissao_idx` (`id_permissao`),
+  CONSTRAINT `fk_id_grupoutilizador` FOREIGN KEY (`id_grupoutilizador`) REFERENCES `grupo_utilizador` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_permissao` FOREIGN KEY (`id_permissao`) REFERENCES `permissao` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grupo_utilizador_permissao`
+--
+
+LOCK TABLES `grupo_utilizador_permissao` WRITE;
+/*!40000 ALTER TABLE `grupo_utilizador_permissao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grupo_utilizador_permissao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `localidade`
+--
+
+DROP TABLE IF EXISTS `localidade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `localidade` (
+  `Id` int(11) NOT NULL,
+  `Id_distrito` int(11) DEFAULT NULL,
+  `Nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_id_distrito_idx` (`Id_distrito`),
+  CONSTRAINT `fk_id_distrito` FOREIGN KEY (`Id_distrito`) REFERENCES `distrito` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `localidade`
+--
+
+LOCK TABLES `localidade` WRITE;
+/*!40000 ALTER TABLE `localidade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `localidade` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido`
+--
+
+DROP TABLE IF EXISTS `pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pedido` (
+  `Id` int(11) NOT NULL,
+  `Id_entidade` int(11) DEFAULT NULL,
+  `Data_entrega` date DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_id_entidade_idx` (`Id_entidade`),
+  CONSTRAINT `fk_id_entidade` FOREIGN KEY (`Id_entidade`) REFERENCES `entidade` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido`
+--
+
+LOCK TABLES `pedido` WRITE;
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido_produto`
+--
+
+DROP TABLE IF EXISTS `pedido_produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pedido_produto` (
+  `Id` int(11) NOT NULL,
+  `Id_pedido` int(11) DEFAULT NULL,
+  `Id_produto` int(11) DEFAULT NULL,
+  `Quantidade` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_id_pedido_idx` (`Id_pedido`),
+  KEY `fk_id_produto_idx` (`Id_produto`),
+  CONSTRAINT `fk_id_pedido` FOREIGN KEY (`Id_pedido`) REFERENCES `pedido` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_produto` FOREIGN KEY (`Id_produto`) REFERENCES `produto` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido_produto`
+--
+
+LOCK TABLES `pedido_produto` WRITE;
+/*!40000 ALTER TABLE `pedido_produto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido_produto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permissao`
+--
+
+DROP TABLE IF EXISTS `permissao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permissao` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(45) NOT NULL,
+  `janela` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissao`
+--
+
+LOCK TABLES `permissao` WRITE;
+/*!40000 ALTER TABLE `permissao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permissao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produto` (
+  `Id` int(11) NOT NULL,
+  `Nome` varchar(45) DEFAULT NULL,
+  `Id_categoria` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_id_categoria_idx` (`Id_categoria`),
+  CONSTRAINT `fk_id_categoria` FOREIGN KEY (`Id_categoria`) REFERENCES `categoria` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produto`
+--
+
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `provincia`
+--
+
+DROP TABLE IF EXISTS `provincia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `provincia` (
+  `Id` int(11) NOT NULL,
+  `Nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `provincia`
+--
+
+LOCK TABLES `provincia` WRITE;
+/*!40000 ALTER TABLE `provincia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `provincia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servico`
+--
+
+DROP TABLE IF EXISTS `servico`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `servico` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servico`
+--
+
+LOCK TABLES `servico` WRITE;
+/*!40000 ALTER TABLE `servico` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servico` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipo_entidade`
+--
+
+DROP TABLE IF EXISTS `tipo_entidade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipo_entidade` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipo_entidade`
+--
+
+LOCK TABLES `tipo_entidade` WRITE;
+/*!40000 ALTER TABLE `tipo_entidade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipo_entidade` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-06-09  2:04:41
